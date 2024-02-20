092ad0a21f9b8b589209dfd7e4e371772f9be528
TYLERSFOSTER <tylerisnotavailable@gmail.com>
2024-02-20 16:36:16 -0500

    -

--- source/simplices_search.py
+++ source/simplices_search.py
@@ -14,6 +14,7 @@ import tower
 Classes
 '''
 class SimplexBot():
+  '''Class representing bot that crawls up HNSW tower, searching for simplices below a fixed dimension'''
   def __init__(self,
                tower_to_search,
                top_search_dimension=1,
@@ -24,3 +25,6 @@ class SimplexBot():
     #-----------------------------------------
     self.tower = tower_to_search
     self.top_dimension = top_search_dimension
+    self.start_index = self.tower.starting_index
+    self.end_index = self.tower.ending_index
+    self.top_sSet = self.tower.tiers[self.start_index].sSet
